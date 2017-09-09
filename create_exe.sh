8cb67ddcf63e6daceab1362180b24a99f7f7b64c
TheZoq2 <frans.skarman@gmail.com>
2017-09-09 12:02:45 +0200

    Add ability to select which theme to change to instead of just toggling

--- create_exe.sh
+++ create_exe.sh
@@ -1,1 +1,4 @@
+#!/bin/bash
+
 python -m zipapp themechanger -p "/bin/python3"
+mv themechanger.pyz theme
--- toggleTheme.py
+++ toggleTheme.py
@@ -1,27 +1,37 @@
 #!/bin/python
+
 import os
 import subprocess
+import sys
 
 import theme
 
+def select_toggled_theme():
+    if not os.path.exists(theme.TMP_FILE_DIR):
+        os.makedirs(theme.TMP_FILE_DIR)
 
-if not os.path.exists(theme.TMP_FILE_DIR):
-    os.makedirs(theme.TMP_FILE_DIR)
+    cTheme = theme.AVAILABLE_THEMES[0]
+    #If there is no current theme
+    if not os.path.isfile(theme.CURRENT_THEME_FILE):
+        f = open(theme.CURRENT_THEME_FILE, 'w')
+        f.write(theme.AVAILABLE_THEMES[0])
+        f.close()
+    else:
+        f = open(theme.CURRENT_THEME_FILE, "r")
+        cTheme = f.readline().replace("\n", "")
+        f.close()
 
-cTheme = theme.AVAILABLE_THEMES[0]
-#If there is no current theme
-if not os.path.isfile(theme.CURRENT_THEME_FILE):
-    f = open(theme.CURRENT_THEME_FILE, 'w')
-    f.write(theme.AVAILABLE_THEMES[0])
-    f.close()
+    #Files exist and a theme has been selected, start changing theme
+    #selecting the next theme
+    return theme.AVAILABLE_THEMES[(theme.AVAILABLE_THEMES.index(cTheme) + 1) % len(theme.AVAILABLE_THEMES)]
+
+if len(sys.argv) != 2:
+    nextTheme = select_toggled_theme()
 else:
-    f = open(theme.CURRENT_THEME_FILE, "r")
-    cTheme = f.readline().replace("\n", "")
-    f.close()
-
-#Files exist and a theme has been selected, start changing theme
-#selecting the next theme
-nextTheme = theme.AVAILABLE_THEMES[(theme.AVAILABLE_THEMES.index(cTheme) + 1) % len(theme.AVAILABLE_THEMES)]
+    nextTheme = sys.argv[1]
+    if not nextTheme in theme.AVAILABLE_THEMES:
+        print("{} is not a valid theme".format(nextTheme))
+        exit(-1)
 
 print(nextTheme)
 
@@ -43,4 +53,3 @@ for cmd in theme.UPDATE_CMDS:
 #Write the new theme
 f = open(theme.CURRENT_THEME_FILE, "w")
 f.write(nextTheme)
-
